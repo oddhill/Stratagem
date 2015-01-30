cfb4ae599ac996cafc11ae44e40a446a5bbbd093
Anton Ranestam <anton@ranestam.se>
2015-01-30 10:48:12 +0100

    Minor touchup.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,11 @@
 (function($) {
 
-// Run whenever the DOM tree is changed, e.g. through AJAX/AHAH
-Drupal.behaviors.ODDBABY = {
-  attach: function (context, settings) {
-
-  }
-}
-
 // Run once when the DOM is ready (page load)
 $(document).ready(function() {
 
-  // If SVG is not supported replace it with png version
-  if(!Modernizr.svg) { /* Check modernizr for svg support */
-    $('img[src*="svg"]').attr('src', function() {
-        return $(this).attr('src').replace('.svg', '.png'); /* Replace suffixes with .png */
-    });
-  }
+	$('a.example').click(function () {
+		$('body').addClass('documenation');
+	});
 
 });
 
